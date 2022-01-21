70a715dca9ccc7620f24685f5c164e132ee2492a
kangkyuseok <90165033+kangkyuseok@users.noreply.github.com>
2022-01-21 18:08:56 +0900

    파일이동

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ app.listen(6077, function(){
 
 
 app.get('/go',function(요청, 응답){
-    응답.send('테스트입니덩')
+    응답.sendFile(__dirname + '/c.html')
 });
 
 
 app.get('/',function(요청, 응답){
     응답.sendFile(__dirname + '/index.html')
 });
+
