296b19d2dc5af0687efa8be3c29171fb2e7aebfa
kangkyuseok <90165033+kangkyuseok@users.noreply.github.com>
2022-02-05 18:43:57 +0900

    node project

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ app.listen(6077, function(){
 });
 
 
-app.get('/go',function(요청, 응답){
-    응답.sendFile(__dirname + '/c.html')
+app.get('/board',function(요청, 응답){
+    응답.sendFile(__dirname + '/board.html')
+});
+
+
+app.get('/test',function(요청, 응답){
+    응답.sendFile(__dirname + '/templates/header.html')
 });
 
 
