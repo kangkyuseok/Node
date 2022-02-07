881cb2f21f4fbacd3b316df931fb8b92e07294df
kangkyuseok <90165033+kangkyuseok@users.noreply.github.com>
2022-02-07 18:12:06 +0900

    static / page not

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public"))  // public 폴더에서 찾음 css 적용 시키기
 
+app.get("/",(req, res) =>{
+    console.log(req.user);
+    res.sendFile(__dirname + "/index.html")
+});
 
-app.listen(6077, function(){
-    console.log('listening on 6077')
+app.listen(6077, function(){          // funtion()을 빼면 => ? 하면되는듯
+    console.log("listening on 6077")
 });
 
 
-app.get('/board',function(req, res){
-    res.sendFile(__dirname + '/template/board.html')
+app.get("/board",(req, res) => {
+    res.sendFile(__dirname + "/templates/board.html")
 });
 
 
-app.get('/test',function(req, res){
-    res.sendFile(__dirname + '/templates/header.html')
+app.get("/test",(req, res) =>{
+    res.sendFile(__dirname + "/templates/header.html")
 });
 
 
-app.get('/',function(req, res){
-    res.sendFile(__dirname + '/index.html')
+
+
+app.get("/v",(req, res) =>{
+    res.sendFile(__dirname + "/templates/videolist.html")
 });
 
-app.get('/v',function(req, res){
-    res.sendFile(__dirname + '/templates/videolist.html')
+app.get("/hello", (req, res) => {
+    res.send("테스트")
 });
 
-app.get('/hello', (req, res) => {
-    res.send('테스트')
+app.use((req, res) => {
+    res.sendFile(__dirname + "/404.html");
 });
+
