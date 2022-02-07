86ada9e1c86377403ced8c3d4df7ea2686cf235b
kangkyuseok <90165033+kangkyuseok@users.noreply.github.com>
2022-02-07 12:55:14 +0900

    오호!!

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 const express = require('express');
+const res = require('express/lib/response');
 const app = express();
+
+
 
 app.listen(6077, function(){
     console.log('listening on 6077')
 });
 
 
-app.get('/board',function(요청, 응답){
-    응답.sendFile(__dirname + '/board.html')
+app.get('/board',function(req, res){
+    res.sendFile(__dirname + '/template/board.html')
 });
 
 
-app.get('/test',function(요청, 응답){
-    응답.sendFile(__dirname + '/templates/header.html')
+app.get('/test',function(req, res){
+    res.sendFile(__dirname + '/templates/header.html')
 });
 
 
-app.get('/',function(요청, 응답){
-    응답.sendFile(__dirname + '/index.html')
+app.get('/',function(req, res){
+    res.sendFile(__dirname + '/index.html')
 });
 
-app.get('/v',function(요청, 응답){
-    응답.sendFile(__dirname + '/templates/videolist.html')
+app.get('/v',function(req, res){
+    res.sendFile(__dirname + '/templates/videolist.html')
 });
 
-
+app.get('/hello', (req, res) => {
+    res.send('테스트')
+});
